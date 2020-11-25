654329c8d6e2cc01c3ce5a58fedcf7953bc68804
Emil <e.lech00@wp.pl>
2020-11-25 19:04:41 +0100

    index view added

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from loan_helper.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', IndexView.as_view())
 ]
