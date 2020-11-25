d3d6c878e96de7b49d506517ba5ecff5014a6ab1
Emil <e.lech00@wp.pl>
2020-11-25 19:37:41 +0100

    client create view added

--- loan_helper/views.py
+++ loan_helper/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from loan_helper.models import Client
 
 
 class IndexView(View):
 
     def get(self, request):
         return render(request, "index.html")
+
+
+class ClientCreate(CreateView):
+    model = Client
+    fields = '__all__'
+    success_url = reverse_lazy('/client/')
+
+
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from loan_helper.views import IndexView
+from loan_helper.views import IndexView, ClientCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', IndexView.as_view())
+    path('', IndexView.as_view()),
+    path('add_client/', ClientCreate.as_view()),
 ]
