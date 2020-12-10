c5b9d6062aa2f733e7de408152ed445bcbc4e4a8
Emil <e.lech00@wp.pl>
2020-12-10 18:04:15 +0100

    documentation to views added

--- loan_helper/views.py
+++ loan_helper/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView, FormView
-from django.urls import reverse_lazy
 from loan_helper.models import Client, Broker, Comment, Occupation, ClientOccupation, Bank, SuccessfulLoan, \
     CURRENT_STATUS, OCCUPATION
 from loan_helper.forms import AddClientForm, UpdateClientForm, LoginForm
@@ -14,11 +13,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LoginView(FormView):
+    """
+    A class used to represent a Login View. It uses custom form.
+    """
+
     form_class = LoginForm
     template_name = "login.html"
     success_url = "/"
 
     def form_valid(self, form):
+        """
+        This method checks if user is authenticated, if is, it allows to log in.
+        If not, user will see error text.
+        """
+
         username = form.cleaned_data["login"]
         password = form.cleaned_data["password"]
         user = authenticate(self.request, username=username, password=password)
@@ -30,14 +38,28 @@ class LoginView(FormView):
 
 
 class LogoutView(View):
+    """
+    A class used to represent a Logout. It does not need template.
+    """
+
     def get(self, request):
+        """
+        This method logout user and redirect him to index.
+        """
         logout(request)
         return redirect("/")
 
 
 class IndexView(View):
+    """
+    A class used to represent a Index View.
+    """
 
     def get(self, request):
+        """
+        This method render index template and checks if user is authenticated. User is passes in context.
+        """
+
         user = request.user
         if user.is_authenticated:
             user = User.objects.get(username='root')
@@ -48,6 +70,10 @@ class IndexView(View):
 
 
 class ClientCreate(LoginRequiredMixin, CreateView):
+    """
+    A class used to represent a Client Create View. It allows to add client to database through custom form.
+    """
+
     model = Client
     success_url = '/all_clients/'
     form_class = AddClientForm
@@ -55,6 +81,10 @@ class ClientCreate(LoginRequiredMixin, CreateView):
 
 
 class BrokerCreate(LoginRequiredMixin, CreateView):
+    """
+    A class used to represent a Broker Create View. It allows to add broker to database through template form.
+    """
+
     model = Broker
     fields = '__all__'
     success_url = '/all_brokers/'
@@ -62,6 +92,10 @@ class BrokerCreate(LoginRequiredMixin, CreateView):
 
 
 class BankCreate(LoginRequiredMixin, CreateView):
+    """
+    A class used to represent a Bank Create View. It allows to add bank to database through template form.
+    """
+
     model = Bank
     fields = '__all__'
     success_url = '/all_banks/'
@@ -69,6 +103,11 @@ class BankCreate(LoginRequiredMixin, CreateView):
 
 
 class SuccessfulLoanCreate(LoginRequiredMixin, CreateView):
+    """
+    A class used to represent a Successful Loan Create View.
+    It allows to add successful loan to database through template form.
+    """
+
     model = SuccessfulLoan
     fields = '__all__'
     success_url = '/all_loans/'
@@ -76,12 +115,21 @@ class SuccessfulLoanCreate(LoginRequiredMixin, CreateView):
 
 
 class ClientListView(LoginRequiredMixin, ListView):
+    """
+    A class used to represent a Client List View. It shows all client from database with specific pagination.
+    Clients are ordered by date created.
+    """
+
     model = Client
     ordering = 'date_created'
     paginate_by = 15
     login_url = '/login/'
 
     def get_context_data(self, **kwargs):
+        """
+        This method return specific context. At this point only ClientOccupation objects are passed.
+        """
+
         context = super(ClientListView, self).get_context_data(**kwargs)
         context.update({
             'client_occupation': ClientOccupation.objects.all(),
@@ -89,6 +137,10 @@ class ClientListView(LoginRequiredMixin, ListView):
         return context
 
     def get_ordering(self):
+        """
+        This method allows to use sorting. It overwrite default ordering.
+        """
+
         order = self.request.GET.get('order')
         sort = self.request.GET.get('sort')
         if sort:
@@ -106,6 +158,10 @@ class ClientListView(LoginRequiredMixin, ListView):
                 return ordering
 
     def get_queryset(self):
+        """
+        This method allows to use search engine. Search engine looks for specific words in main fields.
+        """
+
         search = self.request.GET.get('search')
         data_search = self.request.GET.get('data_search')
         object_list = self.model.objects.all()
@@ -126,6 +182,11 @@ class ClientListView(LoginRequiredMixin, ListView):
 
 
 class BrokerListView(LoginRequiredMixin, ListView):
+    """
+    A class used to represent a Broker List View. It shows all brokers from database with specific pagination.
+    Brokers are ordered by name.
+    """
+
     model = Broker
     ordering = ['name']
     paginate_by = 15
@@ -133,18 +194,32 @@ class BrokerListView(LoginRequiredMixin, ListView):
 
 
 class BankListView(LoginRequiredMixin, ListView):
+    """
+    A class used to represent a Bank List View. It shows all banks from database.
+    Banks are ordered by name.
+    """
+
     model = Bank
     ordering = ['name']
     login_url = '/login/'
 
 
 class SuccessfulLoanListView(LoginRequiredMixin, ListView):
+    """
+    A class used to represent a Successful Loan List View. It shows all loans from database with specific pagination.
+    Loans are ordered by date created descending.
+    """
+
     model = SuccessfulLoan
-    ordering = ['-date_created']
+    ordering = '-date_created'
     paginate_by = 15
     login_url = '/login/'
 
     def get_ordering(self):
+        """
+        This method allows to use sorting. It overwrite default ordering.
+        """
+
         order = self.request.GET.get('order')
         sort = self.request.GET.get('sort')
         if sort:
@@ -163,9 +238,17 @@ class SuccessfulLoanListView(LoginRequiredMixin, ListView):
 
 
 class ClientDetailsView(LoginRequiredMixin, View):
+    """
+    A class used to represent a Client Details View. It shows all information about chosen client.
+    """
+
     login_url = '/login/'
 
     def get(self, request, client_id):
+        """
+        This method allows to pass context. It renders proper template.
+        """
+
         client = Client.objects.get(id=client_id)
         news = Comment.objects.filter(client_id=client_id)
         client_occupation = ClientOccupation.objects.filter(client=client)
@@ -181,6 +264,10 @@ class ClientDetailsView(LoginRequiredMixin, View):
         return render(request, 'client_details.html', ctx)
 
     def post(self, request, client_id):
+        """
+        This method allows to manage comments section and delete client. It passes context and renders proper template.
+        """
+
         client = Client.objects.get(id=client_id)
         news = Comment.objects.filter(client_id=client_id)
         client_occupation = ClientOccupation.objects.filter(client=client)
@@ -226,9 +313,17 @@ class ClientDetailsView(LoginRequiredMixin, View):
 
 
 class BrokerDetailsView(LoginRequiredMixin, View):
+    """
+    A class used to represent a Broker Details View. It shows all information about chosen broker.
+    """
+
     login_url = '/login/'
 
     def get(self, request, broker_id):
+        """
+        This method allows to pass context. It renders proper template.
+        """
+
         broker = Broker.objects.get(id=broker_id)
         all_clients = Client.objects.filter(broker=broker)
         all_loans = SuccessfulLoan.objects.filter(broker=broker)
@@ -241,37 +336,52 @@ class BrokerDetailsView(LoginRequiredMixin, View):
 
         return render(request, 'broker_details.html', ctx)
 
-    def post(self, request, broker_id):
-        return render(request, 'broker_details.html')
-
 
 class ClientUpdate(LoginRequiredMixin, UpdateView):
+    """
+    A class used to represent a Client Update View. It allows to change the same field as Create View.
+    """
+
     model = Client
     pk_url_kwarg = 'pk'
     form_class = UpdateClientForm
     login_url = '/login/'
 
     def form_valid(self, form):
-        """If the form is valid, redirect to the supplied URL."""
+        """
+        If the form is valid, update db and redirect to the supplied URL.
+        """
+
         client_id = self.object.id
         self.object.save()
         return HttpResponseRedirect(f'/client_details/{client_id}')
 
 
 class BrokerUpdate(LoginRequiredMixin, UpdateView):
+    """
+    A class used to represent a Broker Update View. It allows to change the same field as Broker View.
+    """
+
     model = Broker
     pk_url_kwarg = 'pk'
     fields = '__all__'
     login_url = '/login/'
 
     def form_valid(self, form):
-        """If the form is valid, redirect to the supplied URL."""
+        """
+        If the form is valid, update db and redirect to the supplied URL.
+        """
+
         broker_id = self.object.id
         self.object.save()
         return HttpResponseRedirect(f'/client_details/{broker_id}')
 
 
 class LoanUpdate(LoginRequiredMixin, UpdateView):
+    """
+    A class used to represent a Broker Update View. It allows to change the same field as Broker View.
+    """
+
     model = SuccessfulLoan
     pk_url_kwarg = 'pk'
     fields = '__all__'
@@ -280,9 +390,18 @@ class LoanUpdate(LoginRequiredMixin, UpdateView):
 
 
 class ClientOccupationCreate(LoginRequiredMixin, View):
+    """
+    A class used to represent a ClientOccupation Create View.
+    It allows to add or remove specific occupation and income to client and save it to db. It uses custom form.
+    """
+
     login_url = '/login/'
 
     def get(self, request, client_id):
+        """
+        This method allows to pass context. It renders proper template.
+        """
+
         client = Client.objects.get(id=client_id)
         client_occupation = ClientOccupation.objects.filter(client=client)
 
@@ -295,6 +414,10 @@ class ClientOccupationCreate(LoginRequiredMixin, View):
         return render(request, 'income.html', ctx)
 
     def post(self, request, client_id):
+        """
+        This method allows to manage client's occupation. It passes context and renders proper template.
+        """
+
         client = Client.objects.get(id=client_id)
         client_occupation = ClientOccupation.objects.filter(client=client)
 
